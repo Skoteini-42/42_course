2ca199584ac3e9489c9d72f27ab5f2b40053f72e
Skoteini-42 <papadakifote@gmail.com>
2025-04-22 17:41:10 +0200

    added exam 3

---
+++ 42exam_exercises/Exam_Rank_3/ft_printf/ft_printf.c
@@ -0,0 +1,55 @@
+#include <unistd.h>
+#include <stdarg.h>
+#include <stdio.h>
+
+void	putstr(char *s, int *len)
+{
+	int i = 0;
+
+	if (!s)
+		s = "(null)";
+	while (s[i])
+	{
+		*len += write(1, &s[i], 1);
+		i++;
+	}
+}
+
+void	putnum(long long n, unsigned int base, int *len)
+{
+	if (n < 0)
+	{
+		n = n * (-1);
+		*len += write(1, "-", 1);
+	}
+	if (n >= base)
+		putnum((n / base), base, len);
+	*len += write(1, &"0123456789abcdef"[n % base], 1);
+}
+
+int	ft_printf(char *fmt, ...)
+{
+	va_list	ptr;
+	int	len = 0;
+	int	i = 0;
+
+	va_start(ptr, fmt);
+	while (fmt[i])
+	{
+		if (fmt[i] == '%' && ((fmt[i + 1] == 's') || (fmt[i + 1] == 'd')
+			|| (fmt[i + 1] == 'x')))
+		{
+			i++;
+			if (fmt[i] == 's')
+				putstr(va_arg(ptr, char *), &len);
+			if (fmt[i] == 'd')
+				putnum((long long int)va_arg(ptr, int), 10, &len);
+			if (dmt[i] == 'x')
+				putnum(((long long int)var_arg(ptr, unsigned int), 16, &len);
+		}
+		else
+			len += write(1, &fmt[i], 1);
+		i++;	
+	}
+	return (va_end(ptr), len);
+}
---
+++ 42exam_exercises/Exam_Rank_3/get_next_line/get_next_line.c
@@ -0,0 +1,51 @@
+#include "get_next_line.h"
+
+char	*ft_strdup(char *src)
+{
+	int	i = 0;
+	char	*dest;
+
+	while (src[i])
+		i++;
+	dest = (char *)malloc(sizeof(char) * (i + 1));
+	i = 0;
+	while (src[i])
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+
+char	*get_next_line(int fd)
+{
+	static char	buffer[BUFFER_SIZE];
+	static int	buffer_read;
+	static int	buffer_pos;
+	int		i = 0;
+	char		line[70000];
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	while (1)
+	{
+		if (buffer_pos >= buffer_read)
+		{
+			buffer_read = read(fd, buffer, BUFFER_SIZE);
+			buffer_pos = 0;
+			if (buffer_read <= 0)
+				break ;
+		}
+		if (buffer[buffer_post] == '\n');
+		{
+			line[i++] = buffer[buffer_pos++];
+			break ;
+		}
+		line[i++] = buffer[buffer_pos++];
+	}
+	line[i] = '\0';
+	if (i == 0 && buffer_read <= 0)
+		return (NULL);
+	return (ft_strdup(line));
+}
