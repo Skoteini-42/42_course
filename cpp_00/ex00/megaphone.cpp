0d88c2f25c39951b4ae9f1eee6e655fb9eca76d0
Skoteini-42 <papadakifote@gmail.com>
2025-07-02 14:56:00 +0200

    fixes in ex00

--- cpp_00/ex00/megaphone.cpp
+++ cpp_00/ex00/megaphone.cpp
@@ -3,17 +3,14 @@
 
 int	main(int argc, char **argv)
 {
-	int	i;
-	int	j;
-
 	if (argc == 1)
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
 	else
 	{
-		i = 1;
+		int	i = 1;
 		while (i < argc)
 		{
-			j = 0;
+			int	j = 0;
 			while (argv[i][j])
 			{
 				std::cout << (char)std::toupper(argv[i][j]);
