4b4896b0baa7b75daddf2cabff24f5798bc73c53
Skoteini-42 <papadakifote@gmail.com>
2025-07-07 14:03:12 +0200

    ex01 done

---
+++ cpp_00/ex01/Contact.cpp
@@ -0,0 +1,56 @@
+#include "Contact.hpp"
+#include <iostream>
+#include <sstream>
+#include <iomanip>
+
+Contact::Contact() : index(-1) {}
+
+Contact	Contact::createContact(int index)
+{
+	Contact c;
+	c.index = index;
+	c.firstName = getInput("First name: ");
+	c.lastName = getInput("Last name: ");
+	c.nickname = getInput("Nickname: ");
+	c.phoneNumber = getInput("Phone number: ");
+	c.darkestSecret = getInput("Darkest secret: ");
+	return (c);
+}
+
+void	Contact::printSearchLine() const
+{
+	std::cout << std::setw(10) << index + 1 << " | "
+			<< std::setw(10) << truncateField(firstName) << " | "
+			<< std::setw(10) << truncateField(lastName) << " | "
+			<< std::setw(10) << truncateField(nickname) << std::endl;
+}
+
+void	Contact::printContact() const
+{
+	std::cout << "First name: " << firstName << "\n"
+			<< "Last name: " << lastName << "\n"
+			<< "Nickname: " << nickname << "\n"
+			<< "Phone number: " << phoneNumber << "\n"
+			<< "Darkest secret: " << darkestSecret << std::endl;
+}
+
+std::string Contact::getInput(const std::string& prompt)
+{
+	std::string input;
+	while (true)
+	{
+		std::cout << prompt;
+		std::getline(std::cin, input);
+		if (std::cin.eof() || !input.empty())
+			break ;
+		std::cerr << "Error: Field cannot be empty!\n";
+	}
+	return (input);
+}
+
+std::string	Contact::truncateField(const std::string& field)
+{
+	if (field.length() > 10)
+		return (field.substr(0, 9) + ".");
+	return (field + std::string(10 - field.length(), ' '));
+}
---
+++ cpp_00/ex01/Contact.hpp
@@ -0,0 +1,29 @@
+#ifndef CONTACT_HPP
+# define CONTACT_HPP
+
+# include <string>
+
+class Contact
+{
+	private:
+
+		int			index;
+		std::string firstName;
+		std::string lastName;
+		std::string nickname;
+		std::string phoneNumber;
+		std::string darkestSecret;
+
+		static std::string getInput(const std::string& prompt);
+		static std::string truncateField(const std::string& field);
+	
+	public:
+
+		Contact();
+		
+		static	Contact createContact(int index);
+		void	printSearchLine() const;
+		void	printContact() const;
+};
+
+#endif
