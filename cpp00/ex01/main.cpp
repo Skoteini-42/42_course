96935bc62af92b2ca5c0aa6ee7e404a456ebb1e1
Foteini Papadaki <papadakifote@gmail.com>
2025-07-30 14:32:33 +0200

    updated cpp00

---
+++ cpp00/ex01/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+#include "PhoneBook.hpp"
+
+int	main()
+{
+	PhoneBook	phonebook;
+	std::string	input;
+
+	while (true)
+	{
+		std::cout << "Please type ADD, SEARCH or EXIT: ";
+		std::getline(std::cin, input);
+		if (std::cin.eof() || input == "EXIT")
+			break ;
+		else if (input == "ADD")
+			phonebook.addContact();
+		else if (input == "SEARCH")
+			phonebook.searchContact();
+	//	else
+	//		continue ;
+	}
+	return (0);
+}
