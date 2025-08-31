c6da242aaa4b9269619a8ce2706f5ed9b9587720
Skoteini-42 <papadakifote@gmail.com>
2025-08-31 17:19:56 +0200

    added more tests

--- cpp04/ex01/main.cpp
+++ cpp04/ex01/main.cpp
@@ -6,32 +6,37 @@
 
 int main(void)
 {
-    std::cout << "=== Array of animals test ===\n" << std::endl;
+    std::cout << "=== Polymorphism test ===\n" << std::endl;
     
     Animal* animals[4];
     animals[0] = new Dog();
     animals[1] = new Dog(); 
     animals[2] = new Cat();
     animals[3] = new Cat();
-    
-    // Test polymorphism
+
     for (int i = 0; i < 4; i++)
     {
         std::cout << animals[i]->getType() << ": ";
         animals[i]->makeSound();
     }
- 
-    // Test deep copy
-    std::cout << "\n=== Deep copy test === \n" << std::endl;
+
+    std::cout << "\n=== Deep copy test copy constuctor=== \n" << std::endl;
     Dog original;
     original.setIdea(0, "I'm the original dog");
     Dog copy = original;
     copy.setIdea(0, "I'm a copy dog");
-    
     std::cout << "Original: " << original.getIdea(0) << std::endl;
     std::cout << "Copy: " << copy.getIdea(0) << std::endl;
-    
-    // Cleanup
+
+    std::cout << "\n=== Deep copy test assignment operator constuctor=== \n" << std::endl;
+    Cat originalcat;
+    Cat copycat;
+    originalcat.setIdea(0, "I'm the original cat");
+    copycat = originalcat;
+    copycat.setIdea(0, "I'm a copy cat");
+    std::cout << "Original cat: " << originalcat.getIdea(0) << std::endl;
+    std::cout << "Copy cat: " << copycat.getIdea(0) << std::endl;
+
     std::cout << "\n === Cleanup === \n" << std::endl;
     for (int i = 0; i < 4; i++)
         delete animals[i];
